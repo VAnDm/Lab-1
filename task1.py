e802010b313c3258f11327c2c02dae2ed5a99b7b
VAnDm <klirens32@gmail.com>
2024-10-25 16:16:13 +0300

    task1

---
+++ task1.py
@@ -0,0 +1,13 @@
+WHITE = '\u001b[47m'
+RED = '\u001b[41m'
+END = '\u001b[0m'
+
+def draw_poland():
+    length = 32
+    width = 10
+    # reccomended size: length = 16x; width = 5x; x%2 == 0
+    colors = [WHITE, RED]
+    for i in range (width):
+        print(f"{colors[i//(width // 2)]}{' ' * length}{END}")
+if __name__ == "__main__":
+    draw_poland()
