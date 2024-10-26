fdcb38298dc643b18cb9f48ff46025a4e3487592
VAnDm <klirens32@gmail.com>
2024-10-26 20:48:09 +0300

    task2

--- task1.py
+++ task1.py
@@ -1,13 +1,18 @@
+# вариант_4_Польша
+
+
 WHITE = '\u001b[48;5;255m'
 RED = '\u001b[48;5;160m'
 END = '\u001b[0m'
 
-def draw_poland():
-    length = 32
-    width = 10
-    # reccomended size: length = 16x; width = 5x; x%2 == 0
+
+def draw_poland(width):
+    length = (width // 5) * 8
+    # рекомендованные размеры: length = 10k; нужно, чтобы получить соотношение длины к ширине 8/5 и белая и красная полосы состояли из одинакового целого числа строк
     colors = [WHITE, RED]
     for i in range (width):
         print(f"{colors[i//(width // 2)]}{' ' * length}{END}")
+
+
 if __name__ == "__main__":
-    draw_poland()
+    draw_poland(10)
---
+++ task2.py
@@ -0,0 +1,81 @@
+# вариант_4_узор_d
+# примечание: возможно продолжение узора отличается от задуманного, но, так как Вы писали продлить его так, чтобы было похоже на ковер, решил его зациклить)
+
+
+BLACK = '\u001b[48;5;16m'
+WHITE = '\u001b[48;5;255m'
+END = '\u001b[0m'
+
+
+def draw_carpet(table_width):
+    # рекомендованные размеры: table_width = 4k + 1; так как узор имеет период 4 по строкам и должен начинаться и заканчиваться сплошной строкой
+    table_length = 2 * table_width
+    carpet_table = []
+
+    # создание пустой таблицы для генерации закрашенных мест
+    for i in range(table_width):
+        carpet_table.append([' '] * table_length)
+    
+    # генерация закрашенных мест
+    row_len = table_length
+    offset = 0
+    for i in range(table_width):
+        for j in range(table_length):
+
+            # генерация сплошных линий
+            if i % 2 == 0:
+                if j >= (table_length - row_len) // 2  and j < table_length - ((table_length - row_len) // 2):
+                    carpet_table[i][j] = '#'
+
+            # генерация клеток в пустых линиях с одной закрашенной клеткой
+            if (i % 4 == 1 and i < table_width // 2) or (i % 4 == 3 and i > table_width // 2):
+                if j == table_length // 2 - 1 or j == table_length // 2:
+                    carpet_table[i][j] = '#'
+        
+            # генерация клеток в пустых линиях с двумя закрашенными клетками    
+            if (i % 4 == 3 and i < table_width // 2) or (i % 4 == 1 and i > table_width // 2):
+                start = j - offset
+                if row_len % 3 == 1:
+                    if start == row_len // 3 - 1 or start == row_len // 3 or start == 2 * row_len // 3 or start == 2 * row_len // 3 + 1:
+                        carpet_table[i][j] = '#'
+                if row_len % 3 == 2:
+                    if start == (row_len // 3) or start == (row_len // 3) + 1 or start == 2 * (row_len // 3) or start == 2 * (row_len // 3) + 1:
+                        carpet_table[i][j] = '#'
+                if row_len % 3 == 0:
+                    if start == (row_len // 3) - 2 or start == (row_len // 3) - 1 or start == 2 * (row_len // 3) or start == 2 * (row_len // 3) + 1:
+                        carpet_table[i][j] = '#'
+        
+        # рассчет сдвига начала закрашенной строки
+        if i < table_width // 2:
+            row_len = row_len - 4
+            offset = offset + 2
+        else:
+            row_len = row_len + 4
+            offset = offset - 2
+
+    # отражение узора относительно главной диагонали для заполнения закрашенных столбцов (учитываем растяжение по горизонтали вдвое)
+    for i in range(table_width):
+        for j in range(table_length):
+            if j % 2 == 0 and carpet_table[i][j] == '#':
+                carpet_table[j // 2][2 * i] = '#'
+                carpet_table[j // 2][2 * i + 1] = '#'
+
+    # вывод цветного узора
+    for i in range(table_width):
+        s = ''
+        fl = 0
+        for j in carpet_table[i]:
+            if j == '#' and fl == 0:
+                fl = 1
+                s = f"{s}{BLACK} "
+            elif j == ' ' and fl == 1:
+                fl = 0
+                s = f"{s}{WHITE} "
+            else:
+                s = f"{s} "
+        s = f"{s}{END}"
+        print(s)
+
+
+if __name__ == "__main__":
+    draw_carpet(21)
